c492edf25004248a78c8509155ae066db1ee9400
kplutchak <kplutchak@ucla.edu>
2014-09-08 16:53:25 -0700

    First code commit

---
+++ src/com/main/hive/MainActivity.java
@@ -0,0 +1,17 @@
+package com.main.hive;
+
+import com.example.hive.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
